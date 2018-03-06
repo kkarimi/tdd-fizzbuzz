5f31aefe796222c68af6b1b6fcab26177b8ec295
Pete Heard <peter_heard01@hotmail.com>
2018-03-06 11:30:01 +0000

    fizzbuzz

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,51 +1,5 @@
 export default class FizzBuzz {
 
-	start = null;
-	end = null;
-	output = [];
-
-	constructor(start, end) {
-		this.start = start;
-		this.end = end;
-	}
-
-	generate() {
-		let inclusiveEnd = this.end + 1;
-		for (let i = this.start; i < inclusiveEnd; i++) {
-			if (this.isMultipleOf3(i) && this.isMultipleOf5(i)) {
-				this.output.push('FizzBuzz');
-			}
-			else if (this.isMultipleOf3(i)) {
-				this.output.push('Fizz');
-			} else if (this.isMultipleOf5(i)) {
-				this.output.push('Buzz');
-			}
-			else {
-				this.output.push(i);
-			}
-
-		}
-		return this;
-	}
-
-	reverseAndDeleteFizzBuzz() {
-		this.output = this.output.reverse().filter((item) => {
-			return item !== 'FizzBuzz';
-		});
-	}
-
-	isMultipleOf3(value) {
-		return this.isMultipleOf(value, 3);
-	}
-
-	isMultipleOf5(value) {
-		return this.isMultipleOf(value, 5);
-	}
-
-	isMultipleOf(value, factor) {
-		return (value % factor === 0)
-	}
-
 }
 
 
